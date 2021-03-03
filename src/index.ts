1e824d61d46de7a889dc63c9fc514bb1503ba249
VRaphael <gogeta02@live.fr>
2021-03-03 21:07:34 +0100

    Modification programme principal

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import chalk from 'chalk';
 
-console.log(chalk.bold(chalk.green(" --- Meuh Meuh --- ")));
+import appartenancePoint from './function/appartenancePoint';
+import calculAire from './function/calculAire';
+import calculGravite from './function/calculGravite';
+import inputValues from './function/inputValues';
+
+console.log(chalk.bold(chalk.green(" --- MeuhMeuh --- ")));
+
+(async () => {
+    let coords = await inputValues();
+    let aire = calculAire(coords);
+    let pointG = calculGravite(coords, aire);
+    let isIn = appartenancePoint(coords, pointG);
+
+    console.log("\n --- Résultats ---");
+    console.log(`Aire : ${aire}`);
+    console.log(`Centre de gravité : (${pointG[0]}, ${pointG[1]})`);
+
+    if(isIn)
+        console.log(chalk.bold(chalk.greenBright("✔ La vache est dans le pré")));
+    else
+        console.log(chalk.bold(chalk.redBright("× La vache est hors du pré")));
+})();
