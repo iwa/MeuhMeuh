6d7af6e04476ba41d5867d0d4935f6d42167e9fd
Pierre <iwaongit@gmail.com>
2021-03-03 16:53:47 +0100

    add own round function

---
+++ src/utils/round.ts
@@ -0,0 +1,8 @@
+export default function round(value: number, precision: number) {
+    let multiplier = Math.pow(10, precision || 0);
+
+    if(value >= 0)
+        return Math.round(value * multiplier) / multiplier;
+    else
+        return Math.ceil(value * multiplier) / multiplier;
+}
