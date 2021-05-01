e47e4da8e77d9338429f57aab3f33dc1f38a2b3f
Pierre <iwaongit@gmail.com>
2021-05-01 17:47:01 +0200

    feat: :wrench: update how appartenancePoint is calculated

--- src/function/appartenancePoint.ts
+++ src/function/appartenancePoint.ts
@@ -5,18 +5,26 @@ export default function appartenancePoint(coords: Map<number, number[]>, pointG:
         let x = (coords.get(i))[0];
         let y = (coords.get(i))[1];
 
-        let x1 = (coords.get(i+1) || coords.get(0))[0];
-        let y1 = (coords.get(i+1) || coords.get(0))[1];
+        let nextX = (coords.get(i+1) || coords.get(0))[0];
+        let nextY = (coords.get(i+1) || coords.get(0))[1];
 
         let GSi = [x - pointG[0], y - pointG[1]];
-        let GSi1 = [x1 - pointG[0], y1 - pointG[1]];
+        let GSi1 = [nextX - pointG[0], nextY - pointG[1]];
 
         let thetaI = Math.acos((GSi[0] * GSi1[0] + GSi[1] * GSi1[1]) / (Math.sqrt(Math.pow(GSi[0], 2) + Math.pow(GSi[1], 2)) * Math.sqrt(Math.pow(GSi1[0], 2) + Math.pow(GSi1[1], 2))));
+        let det = x * nextY - y * nextX;
+
+        if (thetaI < 0 && det > 0)
+            thetaI *= -1;
+
+        if (thetaI > 0 && det < 0)
+            thetaI *= -1;
 
         sum += thetaI;
     }
 
-    if (sum === (2 * Math.PI))
-        return true;
-    return false;
+    sum = Math.round(sum);
+    if (sum === 0)
+        return false;
+    return true;
 }
