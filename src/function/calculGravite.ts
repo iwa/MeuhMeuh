7cf02072432ea8af75b84211e4122be8193968ff
Pierre <iwaongit@gmail.com>
2021-05-08 22:20:31 +0200

    refactor: :truck: rename some var to keep consistency

--- src/function/calculGravite.ts
+++ src/function/calculGravite.ts
@@ -5,11 +5,11 @@ export default function calculGravite(coords: Map<number, number[]>, aire: numbe
         let x = (coords.get(i))[0];
         let y = (coords.get(i))[1];
 
-        let x1 = (coords.get(i + 1) || coords.get(0))[0];
-        let y1 = (coords.get(i + 1) || coords.get(0))[1];
+        let nextX = (coords.get(i + 1) || coords.get(0))[0];
+        let nextY = (coords.get(i + 1) || coords.get(0))[1];
 
-        resX += ((x + x1) * (x * y1 - x1 * y));
-        resY += ((y + y1) * (x * y1 - x1 * y));
+        resX += ((x + nextX) * (x * nextY - nextX * y));
+        resY += ((y + nextY) * (x * nextY - nextX * y));
     }
     resX *= (1 / (6 * aire));
     resY *= (1 / (6 * aire));
